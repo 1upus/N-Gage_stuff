b25d48acc0fa385bd4e9cb8500e1b59aaaf42ed8
manwithnoface <anbarsukov@gmail.com>
2017-12-26 11:35:48 +0300

    + NNG Tomb Rider translation project

---
+++ projects/ngage_TombRaider/gfx/psx_deldata/tric.bat
@@ -0,0 +1,9 @@
+tric tr1psx AMERTIT.RAW AMERTIT.bmp
+tric tr1psx ATLANLOA.RAW ATLANLOA.bmp
+tric tr1psx CRED.RAW CRED.bmp
+tric tr1psx CRED2.RAW CRED2.bmp
+tric tr1psx EGYPTLOA.RAW EGYPTLOA.bmp
+tric tr1psx end.RAW end.bmp
+tric tr1psx GREEKLOA.raw GREEKLOA.bmp
+tric tr1psx Gymload.raw Gymload.bmp
+pause
